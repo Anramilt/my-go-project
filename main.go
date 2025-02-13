dd89844604db1c54bb4f1d9bf64397110f3b626a
Anramilt <41483234+Anramilt@users.noreply.github.com>
2025-02-13 15:08:50 +0700

    New Serv localhost

--- main.go
+++ main.go
@@ -1,9 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	//"io"
+	//"net"
+	"net/http"
+	//"os"
+)
 
 func main() {
-	fmt.Println("Hello, Go!")
-	fmt.Println("Hello, Go!")
-	fmt.Println("Hello, Go!")
+	//fmt.Println("Hello, Go!")
+	//fmt.Println("Hello, Go!")
+	//fmt.Println("Hello, Go!")
+	/*httpRequest := "GET / HTTP/1.1\n" + "Host: golang.org\n\n"
+	conn, err := net.Dial("tcp", "golang.org:80")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
+	//посылает данные
+	if _, err = conn.Write([]byte(httpRequest)); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	io.Copy(os.Stdout, conn)
+	fmt.Println("Done")*/
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World!")
+	})
+	http.ListenAndServe(":80", nil)
 }
